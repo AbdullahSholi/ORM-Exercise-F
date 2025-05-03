1c3c43d1eea23ca6bc8838bfce2097f81a102ef6
AbdullahSholi <groupgroup060@gmail.com>
2025-05-03 10:28:54 +0300

    Create all models

---
+++ RestaurantReservation.Db/models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace RestaurantReservation.Db.models;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public int RestaurantId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    
+    public List<Order> Orders { get; set; }
+    public Restaurant Restaurant { get; set; }
+}
