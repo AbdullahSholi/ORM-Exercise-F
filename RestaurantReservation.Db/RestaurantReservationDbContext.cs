5a33b335a7da6790265175a0005434f8ee9f5e43
AbdullahSholi <groupgroup060@gmail.com>
2025-05-03 07:41:10 +0300

    Initial Commit

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=192.168.1.104,1433;Database=PubDatabase;User=testuser;Password=Sholi@971;TrustServerCertificate=True;");
+    }
+}
