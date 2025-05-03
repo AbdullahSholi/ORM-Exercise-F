cf5798603d45417e9e587a00e9dfb50d494bd006
AbdullahSholi <groupgroup060@gmail.com>
2025-05-03 12:46:08 +0300

    Add main for run the project

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddDbContext<RestaurantReservationDbContext>(options =>
+                options.UseSqlServer("Server=192.168.1.104,1433;Database=RestaurantReservationCore;User=testuser;Password=Sholi@971;TrustServerCertificate=True;"))
+            .BuildServiceProvider();
+        
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<RestaurantReservationDbContext>();
+            
+            await context.Database.MigrateAsync();
 
-Console.WriteLine("Hello, World!");
+            await context.SeedDataAsync();
+        }
+    }
+}
