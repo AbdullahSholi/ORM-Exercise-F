c6d5a8cafa2ab61688e29495a8199ac90bd180f1
AbdullahSholi <groupgroup060@gmail.com>
2025-06-02 01:57:53 +0300

    make list of orders inside employee nullable

--- RestaurantReservation.Db/Models/Employee.cs
+++ RestaurantReservation.Db/Models/Employee.cs
@@ -8,6 +8,6 @@ public class Employee
     public string LastName { get; set; }
     public EmployeePosition Position { get; set; }
 
-    public List<Order> Orders { get; set; } = new();
+    public List<Order>? Orders { get; set; } = new();
     public Restaurant Restaurant { get; set; }
 }
