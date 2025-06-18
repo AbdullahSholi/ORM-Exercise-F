d828ea0a432c6efe6e6867a3b775dfde03423930
AbdullahSholi <groupgroup060@gmail.com>
2025-06-18 05:38:38 +0300

    Swagger Integration for API testing and documentation

--- RestaurantReservation.API/Program.cs
+++ RestaurantReservation.API/Program.cs
@@ -12,6 +12,43 @@ using RestaurantReservation.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Restaurant Reservation API",
+        Description = "API for managing restaurant reservations.",
+    });
+    
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddSwaggerGenNewtonsoftSupport();
+
 builder.Configuration.AddJsonFile("appsettings.json", true, true);
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
@@ -104,6 +141,14 @@ if (!app.Environment.IsDevelopment())
     });
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant Reservation API V1");
+    options.RoutePrefix = "";
+});
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
